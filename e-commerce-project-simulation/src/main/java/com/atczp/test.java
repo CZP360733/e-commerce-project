8649d0da537278c8bbedb3961ae642be26136080
CZP <528926637@qq.com>
2020-09-14 16:26:30 +0800

    小米第一天写的代码

---
+++ e-commerce-project-simulation/src/main/java/com/atczp/test.java
@@ -0,0 +1,16 @@
+package com.atczp;
+
+/**
+ * @author Chen Zhi Ping
+ * @version V1.0.0
+ * @create 2020-09-14
+ * @Copyright blog.mercymodest.com
+ */
+public class test {
+
+    public void programmer1word(){
+        System.out.println("小米写了一天的代码");
+    }
+
+
+}
